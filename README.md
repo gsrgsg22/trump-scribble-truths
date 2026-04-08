110df87db249eb13daa151d5d5a948af50d8e4f1
gsrgsg22 <matcout123@gmail.com>
2026-04-07 20:26:56 -0400

    changed support from twitter to truthsocial

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
-# Trump-tweet
-Turn President Trump's Tweets into  Kids' Scribbles (Crayon)
+
+# Trump Truths (Crayon Font)
+
+Turn President Trump's Truth Social posts into kids' scribbles (crayon font).
+
 <h3>Process to install in Google Chrome as extension : </h3>
 <ul>
 <li> Open Terminal</li>
 <li> git clone or Download zip  </li>
 <li>Copy this link  <strong>chrome://extensions/</strong> and paste in Chrome</li>
-<li>Click <strong>Load unpacked extension</strong> button  and upload the 'show-facebook-alternate-image-tags' folder</li>
+<li>Click <strong>Load unpacked extension</strong> and select this project folder</li>
 <li>Done.</li>
 </ul>
 <p>
 </p>
 
+Screenshots are from Twitter, but this one works for TruthSocial only.
+
 ![Alt text](https://i.imgflip.com/1zazjj.jpg "Screen  Shot 2")
+
+**This extension was originally made by [@kaissaroj](https://github.com/kaissaroj), I only changed the platform from twitter.com to truthsocial.com**
--- web.js
+++ web.js
@@ -1,35 +1,54 @@
+(function () {
+	var style = document.createElement('style');
+	style.type = 'text/css';
+	style.textContent = '@font-face { font-family: i eat crayons; src: url("'
+		+ chrome.runtime.getURL('fonts/i eat crayons.ttf')
+		+ '"); }';
+	(document.head || document.documentElement).appendChild(style);
 
-var fa = document.createElement('style');
-    fa.type = 'text/css';
-    fa.textContent = '@font-face { font-family: i eat crayons; src: url("'
-        + chrome.extension.getURL('fonts/i eat crayons.ttf')
-        + '"); }';
-document.head.appendChild(fa);
-function changeFont(){
-	var content = document.getElementsByClassName('content');
-	for(var i=0; i<content.length;i++){
-		var c = content[i].childNodes[0].nextElementSibling;
-		var username =  c.querySelectorAll('.account-group .username');
-		if(!!username && username[0].innerText == '@realDonaldTrump' ){
-			var tweets = content[i].querySelectorAll('.js-tweet-text-container .TweetTextSize');
-			for(var j =0; j < tweets.length;j++){
-				tweets[j].style.fontFamily = "'i eat crayons', sans-serif";
-				tweets[j].style.color = 'darkred';
-				tweets[j].style.fontSize = '24px';
-			}
+	function isTrumpPost(post) {
+		// Look for link containing /@realDonaldTrump in the post
+		var links = post.querySelectorAll('a[href*="/@realDonaldTrump"]');
+		return links.length > 0;
+	}
+
+	function stylePostText(post) {
+		// Target Truth Social's markup element that contains the post text
+		var textElements = post.querySelectorAll('p[data-testid="markup"]');
+		for (var j = 0; j < textElements.length; j++) {
+			textElements[j].style.fontFamily = "'i eat crayons', sans-serif";
+			textElements[j].style.color = 'darkred';
+			textElements[j].style.fontSize = '24px';
 		}
 	}
-}
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-var observer = new MutationObserver(function(mutations, observer) {
-    // fired when a mutation occurs
-    changeFont();
-    // ...
-});
-// define what element should be observed by the observer
-// and what types of mutations trigger the callback
-observer.observe(document, {
-  subtree: true,
-  attributes: true
-  //...
-});
+
+	function changeFont() {
+		// Look for all post containers (Truth Social uses div[id^="status-"])
+		var posts = document.querySelectorAll('div[id^="status-"]');
+		
+		for (var j = 0; j < posts.length; j++) {
+			if (posts[j].dataset.crayonChecked === '1') {
+				continue;
+			}
+
+			if (isTrumpPost(posts[j])) {
+				stylePostText(posts[j]);
+			}
+
+			posts[j].dataset.crayonChecked = '1';
+		}
+	}
+
+	var Observer = window.MutationObserver || window.WebKitMutationObserver;
+	var observer = new Observer(function () {
+		changeFont();
+	});
+
+	observer.observe(document.body || document.documentElement, {
+		subtree: true,
+		childList: true,
+		attributes: false
+	});
+
+	changeFont();
+})();
